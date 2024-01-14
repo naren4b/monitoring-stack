54f5d6b8e11352626062fa83da3f172feba65ce1
Narendranath Panda <3488520+naren4b@users.noreply.github.com>
2024-01-14 16:34:52 +0530

    Update run-node-exporter.sh

--- run-node-exporter.sh
+++ run-node-exporter.sh
@@ -3,7 +3,7 @@ text_collector_dir=/var/lib/node-exporter/textfile_collector
 mkdir -p ${text_collector_dir}
 
 cat <<EOF >/etc/cron.d/directory_size
-*/5 * * * * root du -sb /var/log /var/cache/apt /var/lib/prometheus | sed -ne 's/^\([0-9]\+\)\t\(.*\)$/node_directory_size_bytes{directory="\2"} \1/p' > ${text_collector_dir}/directory_size.prom.$$ && mv ${text_collector_dir}/directory_size.prom.$$ ${text_collector_dir}/directory_size.prom
+*/5 * * * * root du -sb /var/log /var/cache/apt /var/lib/prometheus | sed -ne 's/^\([0-9]\+\)\t\(.*\)$/node_directory_size_bytes{service="naren",directory="\2"} \1/p' > ${text_collector_dir}/directory_size.prom.$$ && mv ${text_collector_dir}/directory_size.prom.$$ ${text_collector_dir}/directory_size.prom
 EOF
 name=$1
 default_value="demo"
@@ -18,3 +18,4 @@ docker run -d --restart unless-stopped --network host \
     prom/node-exporter --collector.textfile.directory=${text_collector_dir}
 
 docker ps -l
+
