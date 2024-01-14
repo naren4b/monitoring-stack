26af4b503cbe2538f4b90d66f4f2320d6be42d03
Narendranath Panda <3488520+naren4b@users.noreply.github.com>
2024-01-14 16:33:46 +0530

    Update run-vmagent.sh

--- run-vmagent.sh
+++ run-vmagent.sh
@@ -6,20 +6,26 @@ vmagent_name=${name}-vmagent
 mkdir -p ${PWD}/$vmagent_name
 
 remoteWrite_url="http://localhost:8428/api/v1/write"
-cat <<EOF >${PWD}/$vmagent_name/Dockerfile
-FROM victoriametrics/vmagent
-ENTRYPOINT ["/vmagent-prod"]
-CMD ["-remoteWrite.url=$remoteWrite_url" , "-remoteWrite.forceVMProto","-promscrape.config=/etc/prometheus/prometheus.yml"]
-EOF
-
 docker build -t victoriametrics/vmagent:$vmagent_name ${PWD}/$vmagent_name/
 rm -rf ${PWD}/$vmagent_name/Dockerfile
 
+cat <<EOF > ${PWD}/$vmagent_name/relabel.yml
+- target_label: "node"
+  replacement: "local"
+EOF
+
 cat <<EOF >${PWD}/$vmagent_name/prometheus.yml
 scrape_configs:
-  - job_name: prometheus
-    metrics_path: /federate
-    scheme: http
+  - job_name: 'federate'
+    scrape_interval: 15s
+
+    honor_labels: true
+    metrics_path: '/federate'
+
+    params:
+      'match[]':
+        - '{service="naren"}'
+        - '{__name__=~"up|vm_.*"}'
     static_configs:
       - targets:
           - localhost:9090
@@ -31,7 +37,8 @@ docker rm ${vmagent_name} -f
 docker run -d --restart unless-stopped --network host \
   --name=${vmagent_name} \
   -v ${PWD}/$vmagent_name:/etc/prometheus/ \
+  -v ${PWD}/vma:/opt/ \
   -v vmagentdata:/vmagentdata \
-  victoriametrics/vmagent:$vmagent_name
+  victoriametrics/vmagent -remoteWrite.url=$remoteWrite_url -remoteWrite.urlRelabelConfig=/etc/prometheus/relabel.yml -remoteWrite.forceVMProto -promscrape.config=/etc/prometheus/prometheus.yml -remoteWrite.tlsCAFile=/opt/ca.crt -remoteWrite.tlsCertFile=/opt/vmagent-client-tls.crt -remoteWrite.tlsKeyFile=/opt/vmagent-client-tls.key -remoteWrite.tlsInsecureSkipVerify=false
 
 docker ps -l
